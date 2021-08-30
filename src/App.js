0b5b93f7572aac72219ba5ebd1df18954d1cb60b
Nahuel Burban <nahuelbvrban@gmail.com>
2021-08-30 16:30:58 -0300

    primerCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="jumbotron">
+      
+      <h1 className="display-4"> Hello, world!</h1> 
+      <p className="lead">
+        This is a simple hero unit, a simple jumpbotron-style component <br/>
+        for calling extra attention to featured content or information.
+      </p>
+      <hr className="my-4"/>
+      <p>
+        It usses unity classes for typography and spacing to space content out <br/>
+        within the larger container.
+      </p>
+      <a href="ho.a" className="btn btn-primary btn-lg" role="button">Learn More</a>
+
     </div>
   );
 }
